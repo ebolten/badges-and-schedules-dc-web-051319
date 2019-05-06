b0b718eee34ae3d661f95695885f18eb0ddff4d7
Emily Bolten <emilybolten@gmail.com>
2019-05-06 14:44:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,15 @@
 # Write your code here.
 
-def batch_badge_maker(array)
+def badge_maker(array)
   badges = []
 
   for i in 1..array.size
     badges.push("Hello! My name is #{array[i]}.")
   end
+  return badges
 end
+
+def printer(attendees)
+end
+
+def assign_rooms()
