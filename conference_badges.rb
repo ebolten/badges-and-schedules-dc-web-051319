f1dbb40fc77cc0992f946c3b2d2a77f4ae747ec6
Emily Bolten <emilybolten@gmail.com>
2019-05-06 14:53:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,14 +12,24 @@ def batch_badge_creator(array)
   return badges
 end
 
-def printer(attendees)
-end
-
 def assign_rooms(array)
   room_assigns = []
 
   for i in 1..array.size
-    room_assigns.unshift( "Hello, #{array[i]}! You'll be assigned to room #{i}!")
+    room_assigns.unshift("Hello, #{array[i]}! You'll be assigned to room #{i}!")
   end
   return room_assigns
 end
+
+def printer(attendees)
+
+  badges = batch_badge_creator(attendees)
+  room_assigns = assign_rooms(attendees)
+
+  for i in 1..badges.size
+    puts badges[i]
+  end
+
+  for i in 1..room_assigns.size
+    puts room_assigns[i]
+  end
